311418f66c968bdb3c383ec9ff04f427da2e9b20
wutong <heuuLZP@163.com>
2018-02-25 15:13:20 +0800

    requestAnimattionFrame代替setInterval

--- animate.js
+++ animate.js
@@ -1,15 +1,69 @@
 import cssHandle from './css'
 
+// 封装requestAnimationFrame
+if(!window.requestAnimationFrame) {
+    window.requestAnimationFrame = (window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function(callback) {
+        var self = this, start, finish;
+        return window.setTimeout(function() {
+            start = +new Date();
+            callback(start);
+            finish = +new Date();
+            self.timeout = 1000/60 - (finish - start);
+        }, self.timeout);
+    });
+}
+
+// 封装cancelRequestAnimFrame
+window.cancelRequestAnimFrame = ( function() {
+    return window.cancelAnimationFrame ||
+        window.webkitCancelRequestAnimationFrame ||
+        window.mozCancelRequestAnimationFrame ||
+        window.oCancelRequestAnimationFrame ||
+        window.msCancelRequestAnimationFrame ||
+        clearTimeout;
+} )();
+
+
+/**
+ *      缓动函数
+ *      @param {t}  current time（当前时间）；
+ *      @param {b}  beginning value（初始值）；
+ *      @param {c}  change in value（变化量）；
+ *      @param {d}  duration（持续时间）。
+ **/
+let Tween = {
+    Linear: function(t, b, c, d) { 
+        return c * t / d + b; 
+    },
+    Quad: {
+        easeIn: function(t, b, c, d) {
+            return c * (t /= d) * t + b;
+        },
+        easeOut: function(t, b, c, d) {
+            return -c *(t /= d)*(t-2) + b;
+        },
+        easeInOut: function(t, b, c, d) {
+            if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+            return -c / 2 * ((--t) * (t-2) - 1) + b;
+        }
+    }
+}
+
 /** 
  * 
- * 参数
- *      obj   dom元素
  *      json  样式或属性 如果是数字，需要是number  背景色需要rgb格式   例如 rgb(255, 192, 203)  
  *      fn    回调函数
+ *      @param {obj}         element   运动对象，必选
+ *      @param {json}        target    属性：目标值，必选    样式或属性 如果是数字，需要是number  背景色需要rgb格式   例如 rgb(255, 192, 203)  
+ *      @param {fn}          callback  可选，回调函数，链式动画
  **/
 export default function animate(obj,json,fn){
-    clearInterval(obj.timer); 
-    obj.timer=setInterval(function(){
+    cancelRequestAnimFrame(obj.timer); 
+    let step = () => {
         let flag=true;//假设所有的运动都到达终点
         for(let attr in json){
             //1取当前值
@@ -63,10 +117,13 @@ export default function animate(obj,json,fn){
             } 
         }       
         if(flag){
-            clearInterval(obj.timer);
+            cancelRequestAnimFrame(obj.timer);
             if (fn) {
                 fn();
             } 
+        } else {
+            obj.timer = requestAnimationFrame(step);
         }
-    },30);
+    }
+    obj.timer = requestAnimationFrame(step);
 }
